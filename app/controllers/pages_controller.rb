00b8a67fc50cce64f0ef2e9f060ce057dfd31115
sandra yukna <sandy.blue.bee@gmail.com>
2023-07-05 17:28:07 +0200

    creating posts work

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,13 +4,17 @@ class PagesController < ApplicationController
   def home
     @users = User.all
     @feed = Post.all
+    @user = current_user
+    @post = Post.new
   end
 
   def profile
     if params[:format]
       @user = User.find(params[:format])
+      @post = Post.new
     else
       @user = current_user
+      @post = Post.new
     end
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,18 @@
 class PostsController < ApplicationController
+  def create
+    @user = current_user
+    @post = Post.new(post_params)
+    @post.user = @user
+    if @post.save
+      redirect_to profile_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
