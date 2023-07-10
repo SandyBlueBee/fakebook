eed9150c4cfd7f354774fa41a217156733e54158
sandra yukna <sandy.blue.bee@gmail.com>
2023-07-10 18:43:11 +0200

    added likes to comments

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get 'profile', to: 'pages#profile'
   put '/post/:id/like', to: 'posts#like', as: 'post_like'
   delete '/post/:id/unlike', to: 'posts#unlike', as: 'post_unlike'
+  put '/comment/:id/like', to: 'comments#like', as: 'comment_like'
+  delete '/comment/:id/unlike', to: 'comments#unlike', as: 'comment_unlike'
   resources :posts, only: [:index, :create] do
     resources :comments, only: [:create, :destroy]
   end
