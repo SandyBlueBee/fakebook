8143a7189b05aa17ae7035003672e517fc5be2a2
yoann morillo <yoann.morillo@hotmail.fr>
2023-07-11 18:04:35 +0200

    seed + photo avatar

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,16 +9,35 @@
 Message.destroy_all
 puts "Creating users..."
 User.destroy_all
-User.create!(first_name: "Yoann", last_name: "Morillo", gender: "male", email:"yoann@test.com", password: "azerty")
+# User.create!(first_name: "Yoann", last_name: "Morillo", gender: "male", email:"yoann@test.com", password: "azerty")
 # User.create!(first_name: "Youcef", last_name: "Haraghi", gender: "male", email:"youcef@test.com", password: "azerty")
-User.create!(first_name: "Sandy", last_name: "Blue", gender: "female", email:"sandy@test.com", password: "azerty")
+# User.create!(first_name: "Sandy", last_name: "Blue", gender: "female", email:"sandy@test.com", password: "azerty")
+file_yoann = URI.open("https://res.cloudinary.com/dsu8pswsx/image/upload/v1689088770/development/Fakebook/Git_wmkenh.jpg")
+yoann = User.new(
+  email: "yoann@test.com",
+  password: "azerty",
+  first_name: "Yoann",
+  last_name: "Morillo"
+)
+yoann.avatar.attach(io: file_yoann, filename: "Git_wmkenh.jpg", content_type: "image/jpg")
+yoann.save
+
+file_sandy = URI.open("https://res.cloudinary.com/dsu8pswsx/image/upload/v1689088764/development/Fakebook/FB_IMG_1591652523870_harncz.jpg")
+sandy = User.new(
+  email: "sandy@test.com",
+  password: "azerty",
+  first_name: "sandy",
+  last_name: "yukna"
+)
+sandy.avatar.attach(io: file_sandy, filename: "FB_IMG_1591652523870_harncz.jpg", content_type: "image/jpg")
+sandy.save
 
 puts "Creating posts..."
 Post.destroy_all
 
-Post.create!(content: "Hi SANNDDYYYYY", user: User.first)
-Post.create!(content: "Hey guys! I'm new on Fakebook. Anybody have recommendations for new features?", user: User.last)
-Post.create!(content: "Yoann, stop messing with me! :)", user: User.last)
+Post.create!(content: "Hi SANNDDYYYYY", user_id: yoann.id)
+Post.create!(content: "Hey guys! I'm new on Fakebook. Anybody have recommendations for new features?", user_id: sandy.id)
+Post.create!(content: "Yoann, stop messing with me! :)", user_id: sandy.id)
 
 
 
