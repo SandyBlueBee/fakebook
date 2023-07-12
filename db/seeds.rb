35e0fe7d6a1d7cbb7fe09e11e096eca59f6fb176
elise rochaix <elise.rochaix@gmail.com>
2023-07-13 00:53:48 +0200

    devise css

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,13 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
 
 Message.destroy_all
-puts "Creating users..."
 User.destroy_all
+Post.destroy_all
+
+puts "Creating users..."
 # User.create!(first_name: "Yoann", last_name: "Morillo", gender: "male", email:"yoann@test.com", password: "azerty")
 # User.create!(first_name: "Youcef", last_name: "Haraghi", gender: "male", email:"youcef@test.com", password: "azerty")
 # User.create!(first_name: "Sandy", last_name: "Blue", gender: "female", email:"sandy@test.com", password: "azerty")
@@ -33,7 +32,6 @@ sandy.avatar.attach(io: file_sandy, filename: "FB_IMG_1591652523870_harncz.jpg",
 sandy.save
 
 puts "Creating posts..."
-Post.destroy_all
 
 Post.create!(content: "Hi SANNDDYYYYY", user_id: yoann.id)
 Post.create!(content: "Hey guys! I'm new on Fakebook. Anybody have recommendations for new features?", user_id: sandy.id)
