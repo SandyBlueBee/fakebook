86ecc35ecefaec9888d7572fc6e03f6d00d83f83
sandra yukna <sandy.blue.bee@gmail.com>
2023-07-15 19:28:18 +0200

    solved comment button

--- app/javascript/controllers/posts_controller.js
+++ app/javascript/controllers/posts_controller.js
@@ -9,19 +9,11 @@ export default class extends Controller {
 
   commentShow() {
     // console.log("show comments")
-    if (this.commentshowTarget.style.display === "none") {
-      this.commentshowTarget.style.display = "flex";
-      this.commentformTarget.style.display = "flex";
-    } else {
-      this.commentshowTarget.style.display = "none";
-      this.commentformTarget.style.display = "none";
-    }
+    this.commentshowTarget.classList.toggle("visible");
+    this.commentformTarget.classList.toggle("visible");
   }
 
   commentNew() {
-    console.log(this.commentformTarget)
-      this.commentshowTarget.classList.toggle("visible");
-      this.commentformTarget.classList.toggle("visible");
-
+    this.commentformTarget.classList.toggle("visible");
   }
 }
