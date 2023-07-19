c36a2671b8f9bf85ef249ed2b8217a603a5ba524
youcef haraghi <haraghiy@gmail.com>
2023-07-19 17:37:11 +0200

    add emoji to form

---
+++ app/javascript/controllers/emoji_controller.js
@@ -0,0 +1,27 @@
+import { Controller } from "@hotwired/stimulus"
+import { EmojiPickerElement } from 'unicode-emoji-picker';
+// Connects to data-controller="emoji"
+export default class extends Controller {
+  static targets = ["emoji", "input"]
+  connect() {
+    console.log("Emoji is in the House!");
+  }
+    add(event) {
+      event.preventDefault();
+        console.log(event);
+        console.log("l'emoji", event.detail.emoji);
+        let start_position = this.inputTarget.selectionStart;
+        let end_position = this.inputTarget.selectionEnd;
+        this.inputTarget.value = this.inputTarget.value.substring(0, start_position) + event.detail.emoji + this.inputTarget.value.substring(end_position);
+
+        // this.inputTarget.value += event.detail.emoji;
+        this.emojiTarget.classList.add("hidden");
+        console.log("le form",this.inputTarget);
+        console.log("la valeur de l'input",this.inputTarget.value);
+
+    }
+
+    togglePicker() {
+      this.emojiTarget.classList.toggle("hidden");
+    }
+  }
