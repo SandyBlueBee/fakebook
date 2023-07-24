2d59094b0f3b6ddffd677653be42e1e04eb12f13
youcef haraghi <haraghiy@gmail.com>
2023-07-24 18:25:30 +0200

    last

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -25,7 +25,6 @@ class MessagesController < ApplicationController
   def notify_recipient
     @recipient = @chatroom.users.reject { |user| user == current_user }
     MessageNotification.with(message: "Un nouveau post a été créé !", sender: current_user).deliver(@recipient)
-    # Add sender to message notification
 
   end
 
